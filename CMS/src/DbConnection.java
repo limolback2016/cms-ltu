5c6972696fb3a11f9625a1e0aaf43d80a4d5fc5a
Li M <limolback@hotmail.se>
2017-05-09 22:38:42 +0200

    [task] lägg till projekt på cms-ltu repo

---
+++ CMS/src/DbConnection.java
@@ -0,0 +1,21 @@
+
+/*
+ * This class will handler connection 
+ * between database and application
+ */
+
+public class DbConnection {
+	private String serverAddress;
+	private String port;
+	private String user;
+	private String password;
+	
+	public void connect(){
+		;
+	}
+	
+	public void disconnect(){
+		;
+	}
+}
+
---
+++ CMS/src/DbHandler.java
@@ -0,0 +1,35 @@
+
+import java.util.Properties;
+
+public class DbHandler {
+	private Properties setting;
+	
+	public void connect(){
+		;
+	}
+	
+	public void createTask(){
+		;
+	}
+	
+	public void updateTask(){
+		;
+	}
+	
+	public void deleteTask(){
+		;
+	}
+	
+	public void createSubtask(){
+		;
+	}
+	
+	public void updateSubtask(){
+		;
+	}
+	
+	public void deleteSubtask(){
+		;
+	}
+
+}
